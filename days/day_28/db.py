1c1e6ed6ead02d5c4132fdb3ac2cb6e0de51cd76
zendix <2013alievgleb@gmail.com>
2026-05-30 12:58:50 +0300

    daily update

--- days/day_28/db.py
+++ days/day_28/db.py
@@ -5,4 +5,7 @@ def add_user(username, role):
     if not path.exists():
         with open(path, "w", encoding="utf-8") as file:
             json.dump({}, file, ensure_ascii=False, indent=4)
+    else:
+        with open(path, "w", encoding="utf-8") as file:
+            json.dump({username:role}, file, ensure_ascii=False, indent=4)
 add_user("zen-dix", "admin")

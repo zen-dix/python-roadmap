0ab6aea53103001b55bb2d0a403d465af762f330
zendix <2013alievgleb@gmail.com>
2026-06-10 14:26:38 +0300

    daily update

--- days/day_35/task5.py
+++ days/day_35/task5.py
@@ -1,1 +1,13 @@
-def
+import time
+def delay_execution(seconds):
+    def dec(func):
+        def wrapped(*args, **kwargs):
+            time.sleep(seconds)
+            return func(*args, **kwargs)
+        return wrapped
+    return dec
+@delay_execution(seconds=2)
+def fetch_data():
+    return "Data fetched!"
+
+print(fetch_data())

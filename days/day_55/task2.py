a5345e98c811369128d9b3fd99a3511bbb62fca7
zendix <2013alievgleb@gmail.com>
2026-08-27 11:05:32 +0300

    Complete task 2

---
+++ days/day_55/task2.py
@@ -0,0 +1,33 @@
+"""2. Concurrent rewrite (7 мин)
+Перепиши задачу 1 так, чтобы обе функции запускались конкурентно через asyncio.create_task.
+Ожидаемый результат: время ≈ 1.5s (максимум из задержек, а не сумма).
+    В конце добавь однострочный английский print-комментарий, поясняющий, почему стало быстрее."""
+
+import asyncio
+import time
+
+
+async def download_avatar():
+    print("start download avatar")
+    await asyncio.sleep(1.5)
+    print("avatar download succeful")
+
+
+async def download_settings():
+    print("start download settings")
+    await asyncio.sleep(1)
+    print("settings download succeful")
+
+
+async def main():
+    print("concurrency")
+    task1 = asyncio.create_task(download_avatar())
+    task2 = asyncio.create_task(download_settings())
+    start = time.monotonic()
+    result1 = await task1
+    result2 = await task2
+    end = time.monotonic()
+    print(round(end - start, 1), "s")
+
+
+asyncio.run(main())
